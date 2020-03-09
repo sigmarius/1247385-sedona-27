c2a589630ba109fc43fc70cfc8d49b9abf096349
sigmarius <aamokeeva@yandex.ru>
2020-03-09 19:29:24 +0300

    Исправляет ошибки JS onDOMContentLoaded

--- js/script.js
+++ js/script.js
@@ -1,74 +1,68 @@
 (function () {
-var link = document.querySelector(".js-link");
-var form = document.querySelector(".js-form");
+  document.addEventListener("DOMContentLoaded", function () {
+    var link = document.querySelector(".js-link");
+    var form = document.querySelector(".js-form");
 
-var arrivalDate = form.querySelector("[name=arrival-date]");
-var livingDate = form.querySelector("[name=living-date]");
-var adultsAmount = form.querySelector("[name=adults-amount]");
-var kidsAmount = form.querySelector("[name=kids-amount]");
+    var arrivalDate = form.querySelector("[name=arrival-date]");
+    var livingDate = form.querySelector("[name=living-date]");
+    var adultsAmount = form.querySelector("[name=adults-amount]");
+    var kidsAmount = form.querySelector("[name=kids-amount]");
 
-var isStorageSupport = true;
-var adults = "";
-var kids = "";
+    var isStorageSupport = true;
+    var adults = "";
+    var kids = "";
 
-var codeEscape = 27;
+    var codeEscape = 27;
 
-var onLinkClick = function (evt) {
-  evt.preventDefault();
-  form.classList.toggle("modal-hidden");
-  form.classList.remove("modal-error");
-  arrivalDate.focus();
-  if (adults && kids) {
-    adultsAmount.value = adults;
-    kidsAmount.value = kids;
-  }
-};
-
-var onSubmitClick = function (evt) {
-  if (!arrivalDate.value || !livingDate.value) {
-    evt.preventDefault();
-    form.classList.remove("modal-error");
-    form.offsetWidth = form.offsetWidth; 
-    form.classList.add("modal-error");
-  } else {
-    if (isStorageSupport) {
-      localStorage.setItem("adults", adultsAmount.value);
-      localStorage.setItem("kids", kidsAmount.value);
+    try {
+      adults = localStorage.getItem("adults");
+      kids = localStorage.getItem("kids");
+    } catch (err) {
+      isStorageSupport = false;
     }
-  }
-};
 
-var onEscapeClick = function (evt) {
-  if (evt.keyCode === codeEscape) {
-    evt.preventDefault();
-    if (!form.classList.contains("modal-hidden")) {
-      form.classList.add("modal-hidden");
+    form.classList.add("modal-hidden");
+   
+    var onLinkClick = function (evt) {
+      evt.preventDefault();
+      form.classList.toggle("modal-hidden");
       form.classList.remove("modal-error");
-    }
-  }
-};
+      arrivalDate.focus();
+      if (adults && kids) {
+        adultsAmount.value = adults;
+        kidsAmount.value = kids;
+      }
+    };
 
-var onDocumentReady = function (evt) {
-  evt.preventDefault();
-  form.classList.add("modal-hidden");
-};
+    var onSubmitClick = function (evt) {
+      if (!arrivalDate.value || !livingDate.value) {
+        // form.classList.remove("modal-error");
+        // form.offsetWidth = form.offsetWidth; 
+        // form.classList.add("modal-error");
+        evt.preventDefault();
+        form.className = "search-form js-form modal-error";    
+      } else {
+        if (isStorageSupport) {
+          localStorage.setItem("adults", adultsAmount.value);
+          localStorage.setItem("kids", kidsAmount.value);
+        }
+      }
+    };
 
-try {
-  adults = localStorage.getItem("adults");
-  kids = localStorage.getItem("kids");
-} catch (err) {
-  isStorageSupport = false;
-}
+    var isKeydownEsc = function (evt) {
+      if (evt.keyCode === codeEscape) {
+        evt.preventDefault();
+        if (!form.classList.contains("modal-hidden")) {
+          form.classList.add("modal-hidden");
+          form.classList.remove("modal-error");
+        }
+      }
+    };
 
-if (document.readyState === "loading") {
-  document.addEventListener("DOMContentLoaded", onDocumentReady);
-} else {
-  onDocumentReady;
-}
+    link.addEventListener("click", onLinkClick);
 
-link.addEventListener("click", onLinkClick);
+    form.addEventListener("submit", onSubmitClick);
 
-form.addEventListener("submit", onSubmitClick);
-
-window.addEventListener("keydown", onEscapeClick);
+    window.addEventListener("keydown", isKeydownEsc);
+  });
 })();
